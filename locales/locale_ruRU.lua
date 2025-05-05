32a1c018e136f610358ea20155833ca94ecfe4c1
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-05-05 19:43:35 +0500

    Create locale_ruRU.lua

---
+++ locales/locale_ruRU.lua
@@ -0,0 +1,42 @@
+local _, ns = ...
+
+if (GetLocale() ~= "ruRU") then return end
+--
+-- Общее
+ns.L                                  = {}
+ns.L["DRT_ADDON_NAME"]                = "Записи Вылазок"
+ns.L["SAVE"]                          = "Сохранить"
+ns.L["DELETE"]                        = "Удалить"
+ns.L["OPERATION"]                     = "Операция"
+ns.L["DRT_RECORDS"]                   = "Записи"
+ns.L["DRT_ADD_RECORD"]                = "Добавить запись"
+ns.L["DRT_SETTING"]                   = "Параметры"
+ns.L["SAVE_SUCCESS"]                  = "|cFF7DDA58Сохранение выполнено успешно!|r"
+ns.L["VISIBLE"]                       = "|cFF7DDA58Видимый|r"
+ns.L["IN_VISIBLE"]                    = "|cFFE4080AНевидимый|r"
+
+-- Записи
+ns.L["BOUNTY_MAP_IN_BAG"]             = "В сумке"
+ns.L["BOUNTY_MAP_USED"]               = "Использовано"
+ns.L["COMPLETE_THE_TOTAL"]            = "Завершено"
+ns.L["BOUNTIFUL"]                     = "Многообещающая Вылазка"
+
+-- Добавление записей
+ns.L["AR_PLAYER_NAME"]                = "Имя игрока"
+ns.L["AR_DELVE_NAME"]                 = "Название Вылазки"
+ns.L["AR_DELVE_TIER"]                 = "Уровень"
+ns.L["AR_SELECT_PLAYER"]              = "Выбрать игрока"
+ns.L["SELECT_PLAYER_AND_DELVE_FIRST"] = "|cFFE4080AСначала выберите соответствующую информацию|r"
+ns.L["AR_DELVE_RECORED"]              = "Записи Вылазки"
+
+-- Настройки
+ns.L["RESET_RECORD"]                  = "Сбросить запись"
+ns.L["RESET_RECORD_CONFIRM"]          = "Следует ли сбросить запись"
+ns.L["LAST_SELECTED_DELVES_TIER"]     = "Последний выбранный уровень Вылазки"
+ns.L["PLAYER_STTING"]                 = "Параметры игрока"
+ns.L["PLAYER_NAME"]                   = "Имя игрока"
+ns.L["PLAYER_VISIBLE"]                = "Видимый"
+ns.L["PLAYER_SORT"]                   = "Сортировка"
+ns.L["PLAYER_DELETE"]                 = "Удалить"
+ns.L["FRAME_SETTING"]                 = "Настройки рамки (вступает в силу после перезагрузки интерфейса)"
+ns.L["FRAME_WIDTH"]                   = "Ширина рамки"
